f1e45a543763623f3f6e3315fc34845a01146ef9
mailtorohini <103950695+mailtorohini@users.noreply.github.com>
2022-10-30 14:33:24 +0530

    first commit

---
+++ Spring-Boot-Web-App/src/test/java/com/phase5/springboot/SpringBootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phase5.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
